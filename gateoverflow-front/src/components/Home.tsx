adea7728a0fda391bc5500d2d854caf365f1fe0c
Sahil-Rana-990 <125109623+Sahil-Rana-990@users.noreply.github.com>
2023-12-25 14:15:49 +0530

    Update Home.tsx

--- gateoverflow-front/src/components/Home.tsx
+++ gateoverflow-front/src/components/Home.tsx
@@ -9,43 +9,43 @@ const Home = () => {
   return (
     <div className='my-10 xs:my-5'>
       <div className='flex flex-col items-center px-6 mx-auto lg:py-0'>
-        <div className='w-[800px] '>
+        <div className='w-[800px] xs:w-[300px]'>
           {/* title for subject */}
-          <div className='text-4xl font-medium font-sans text-gray-500 text-center'>
+          <div className='text-4xl font-medium font-sans text-gray-500 text-center xs:text-[30px]'>
             GateOverFlow Subject
           </div>
 
           {/* subjects */}
-          <div className='p-2 grid grid-cols-2 gap-5 mt-5'>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
-              <div className='text-4xl'><FcGenealogy/></div>
+          <div className='p-2 grid grid-cols-2 gap-5 mt-5 sm:grid-cols-1 xs:grid-cols-1'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
+              <div className='text-4xl xs:text-[10px]'><FcGenealogy/></div>
               <div className='text-center ml-5'>General Aptitude</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><BiMath/></div>
               <div className='text-center ml-5'>Engineering Mathematics</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><TbLogicAnd/></div>
               <div className='text-center ml-5'>Digital Logic</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><BsCodeSlash/></div>
               <div className='text-center ml-5'>Programming and DS</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><TbManualGearbox/></div>
               <div className='text-center ml-5'>Algorithms</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><AiOutlineApartment/></div>
               <div className='text-center ml-5'>Theory of Computation</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><AiFillSetting/></div>
               <div className='text-center ml-5'>Compiler Design</div>
             </div>
-            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500'>
+            <div className='bg-[#36393F] p-5 rounded-md text-white flex items-center hover:bg-purple-700 duration-500 xs:p-4 xs:w-full'>
               <div className='text-4xl'><MdOutlineSettingsSystemDaydream/></div>
               <div className='text-center ml-5'>Operating System</div>
             </div>
